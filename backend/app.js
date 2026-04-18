00ed77370155cd72e25e6b13234d3e98f7c23a1f
haashimanocha <haashima4793.se24@chitkara.edu.in>
2026-04-18 23:00:44 +0530

    added

--- backend/app.js
+++ backend/app.js
@@ -10,10 +10,10 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "../frontend")));
 
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
+
 
 let users = [];
 let tasks = [];
