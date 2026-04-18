a393f579d514761bd840ab82d259e5e6939645ba
haashimanocha <haashima4793.se24@chitkara.edu.in>
2026-04-18 16:15:48 +0530

    fixed backend properly

---
+++ backend/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let users = [];
+let tasks = [];
+
+// Signup
+app.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+  users.push({ username, password });
+  res.send("User registered");
+});
+
+// Login
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    u => u.username === username && u.password === password
+  );
+
+  if (user) {
+    res.json({ message: "Login successful", username });
+  } else {
+    res.status(401).send("Invalid credentials");
+  }
+});
+
+// Get tasks (only for logged user)
+app.get("/tasks", (req, res) => {
+  const username = req.query.username;
+  const userTasks = tasks.filter(t => t.username === username);
+  res.json(userTasks);
+});
+
+// Add task
+app.post("/tasks", (req, res) => {
+  const task = req.body;
+  tasks.push(task);
+  res.json(task);
+});
+
+// Delete task
+app.delete("/tasks/:id", (req, res) => {
+  const id = req.params.id;
+  tasks.splice(id, 1);
+  res.send("Deleted");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log("Server running on port 3000"));
